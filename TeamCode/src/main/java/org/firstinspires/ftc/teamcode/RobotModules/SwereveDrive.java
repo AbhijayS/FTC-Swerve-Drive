ac17dc271c6bddfaeea3978a78aaffc392fe422b
Yazan <yazan@Yazan-MacBook-Air.local>
2019-09-11 17:16:45 -0500

    Update SwereveDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/SwereveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/SwereveDrive.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.RobotModules;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class SwereveDrive {
+    private LinearOpMode linearOpMode;
+    private Telemetry telemetry;
     
 }
