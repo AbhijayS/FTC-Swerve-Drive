33a6b41af424f6f1110ccc4284bdb1e91d0e48d0
Yazan <yazan@Yazan-MacBook-Air.local>
2019-09-11 18:27:18 -0500

    Added a universal constants class with final variable values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/UniversalConstants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.RobotModules;
+
+public class UniversalConstants {
+    public static final String rightFront = "rf";
+    public static final String rightRear = "rr";
+    public static final String leftFront = "lf";
+    public static final String leftRear = "lr";
+    public static final double maxDriveSpeed = 1;
+}
