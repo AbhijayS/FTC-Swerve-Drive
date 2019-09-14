e29c2acc96c6011d76a6897f87ae5cf564b90b6f
FlxMurdo2019 <52217865+FlxMurdo2019@users.noreply.github.com>
2019-09-14 12:22:18 -0500

    added servo control trough x and y

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/CatapultDrivetrainRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/CatapultDrivetrainRunner.java
@@ -4,15 +4,24 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="fondationIntakeTest")
+@TeleOp(name="foundationIntakeTest")
 public class CatapultDrivetrainRunner extends LinearOpMode {
     public void runOpMode() {
         CatapultDrivetrain drivetrain = new CatapultDrivetrain(this, DcMotor.ZeroPowerBehavior.BRAKE);
-        CRServo bob = hardwareMap.crservo.get("s1");
+        CRServo bobTheMover = hardwareMap.crservo.get("s1");
         waitForStart();
         while (opModeIsActive()){
             drivetrain.updateByGamepad();
-            
+            bob.setPosition(0);
+            if (gamepad1.x){
+                bobTheMover.setPower(-.3);
+                wait(100)
+                bobTheMover.setPower(0);
+            }
+            if (gamepade.y) {
+                bobTheMover.setPower(.3);
+                wait(100)
+                bobTheMover.setPower(0);
             }
         }
     }
