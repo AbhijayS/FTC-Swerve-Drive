0f5b171d56d294788679a669362d62d72f9d07c1
Cameron Shugart <shriketurret@gmail.com>
2019-09-16 18:48:41 -0500

    dear future cam please test this thanks ily

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/vuforiaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/vuforiaTest.java
@@ -25,7 +25,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 
 
 @TeleOp(name="vuforiaTest")
-public class yaboiVuforia extends LinearOpMode {
+public class vuforiaTest extends LinearOpMode {
     VuforiaLocalizer vuforia;
     @Override public void runOpMode() {
 
