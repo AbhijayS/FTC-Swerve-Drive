9fcecc593c59274060b9f30d26fcd119368825e0
Gabriel Brown <Gabebrown2023@gmail.com>
2019-09-27 17:38:47 -0500

    im assuming someone didnt push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/CatapultDrivetrainRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aaaaaaaa/CatapultDrivetrainRunner.java
@@ -4,6 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.VEX393Servo;
+
 
 @TeleOp(name="foundationIntakeTest")
 public class CatapultDrivetrainRunner extends LinearOpMode {
@@ -13,12 +17,13 @@ public class CatapultDrivetrainRunner extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()){
             drivetrain.updateByGamepad();
+            servoCheck();
             /**
              * below:
              * if x pressed, then moves the servo down for half a second.
              * if y pressed, then moves servo up for half a second.
              * all this so you can move the foundation in and out of the building zone.
-             */
+             *
             if (gamepad1.x){
                 bobTheMover.setPower(-.3);
                 bobTheMover.setPower(0);
@@ -27,6 +32,31 @@ public class CatapultDrivetrainRunner extends LinearOpMode {
                 bobTheMover.setPower(.3);
                 bobTheMover.setPower(0);
             }
+             **/
+
         }
+    }
+
+    private void servoCheck() {
+        float tgtPower = -this.gamepad1.left_stick_y;
+        VEX393Servo motorTest = null;
+        motorTest.setPower(tgtPower);
+        // check to see if we need to move the servo.
+        Servo servoTest = null;
+        if(gamepad1.y) {
+            // move to 0 degrees.
+            servoTest.setPosition(0);
+        } else if (gamepad1.x) {
+            // move to 90 degrees.
+            servoTest.setPosition(0.5);
+        } else if (gamepad1.left_bumper) {
+            // move to 180 degrees.
+            servoTest.setPosition(1);
+        }
+        telemetry.addData("Servo Position", servoTest.getPosition());
+        telemetry.addData("Target Power", tgtPower);
+        telemetry.addData("Motor Power", motorTest.getPower());
+        telemetry.addData("Status", "Running");
+        telemetry.update();
     }
 }
