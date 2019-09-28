23c5bc224b0619124ff724a5ef3fbaea7e9ea2b0
FlxMurdo2019 <52217865+FlxMurdo2019@users.noreply.github.com>
2019-09-28 12:20:08 -0500

    changed servos to motors then added 2 servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collins_demo_code/collins_drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collins_demo_code/collins_drivetrain.java
@@ -6,29 +6,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 
-@TeleOp(name="foundationIntakeTest")
+@TeleOp(name="collins_drivetrain")
 public class collins_drivetrain extends LinearOpMode {
     
 
     public void runOpMode() {
         collins_drivetrain drivetrain = new collins_drivetrain(this, DcMotor.ZeroPowerBehavior.BRAKE);
-        CRServo bobTheMover = hardwareMap.crservo.get("s1");
+        Motor RightMotor = hardwareMap.Motor.get("m1");
+        Motor LeftMotor = hardwareMap.Motor.get("m2");
+        Servo RightServo = hardwareMap.Servo.get("s1");
+        Servo LeftServo = hardwareMap.Servo.get("s2");
         waitForStart();
         while (opModeIsActive()){
             drivetrain.updateByGamepad();
-            /**
-             * below:
-             * if x pressed, then moves the servo down for half a second.
-             * if y pressed, then moves servo up for half a second.
-             * all this so you can move the foundation in and out of the building zone.
-             */
-            if (gamepad1.x){
-                bobTheMover.setPower(-.3);
-                bobTheMover.setPower(0);
+            if (gamepad1.Trigger1 > 0){
+                RightMotor.setPower(1);
+                LeftMotor.setPower(-1);
             }
-            if (gamepad1.y) {
-                bobTheMover.setPower(.3);
-                bobTheMover.setPower(0);
+            if (gamepad1.Trigger1 = 0) {
+                RightMotore.setPower(0);
+                LeftMotor.setPower(0);
+            }
+            if (gamepad1.x = 1) {
+                RightServo.power(1);
+                LeftServo.power(1);
+            }
+            if (gamepad.y = 1) {
+                RightServo.power(0);
+                LeftServo.power(0);
             }
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grabber_code/grabber1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grabber_code/grabber1.java
@@ -1,5 +1,14 @@
 package org.firstinspires.ftc.teamcode.RobotModules;
 
-public class grabber1 {
-    public Servo hardwareMap.get
+@TeleOp name=("grabber1")
+public class grabber1 extends LinearOpMode {
+    CRServo leftServo = hardwareMap.crservo.get("left");
+    CRServo rightServo = hardwareMap.crservo.get("right");
+
+    waitForStart();
+    while (OpModeIsActive()){
+        if (gamePad1.X
+
+    }
+
 }
