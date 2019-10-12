1c635a8786037dd37fd1eb909c63f3c590de3b15
Cameron Shugart <shriketurret@gmail.com>
2019-10-12 11:15:58 -0500

    somebody didn't push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_arm.java
@@ -8,34 +8,35 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name="collins_drivetrain")
+@TeleOp(name="arm_test")
 
 public class auto_arm extends LinearOpMode {
-    Servo   Servo;
+    Servo Servo;
+
     public void runOpMode() {
         Servo lift_servo = hardwareMap.get(Servo.class, "s1");
         Servo grab_servo = hardwareMap.get(Servo.class, "s2");
+        double position = 0;
         waitForStart();
         // run until the end of the match (driver presses STOP)
         double tgtPower = 0;
         while (opModeIsActive()) {
-            tgtPower = -this.gamepad1.left_stick_y;
-            // check to see if we need to move the servo.
 
-            if(gamepad1.y) {
-                // move to 0 degrees.
-                lift_servo.setPosition(0);
-            } else if (gamepad1.x || gamepad1.b) {
-                // move to 90 degrees.
-                lift_servo.setPosition(0.5);
-            } else if (gamepad1.a) {
-                // move to 180 degrees.
-                lift_servo.setPosition(1);
+            if (gamepad1.y) {
+                position = position + 0.0005;
+//                grab_servo.setPosition(position);
             }
-            telemetry.addData("Servo Position", lift_servo.getPosition());
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", ">>>");
-            telemetry.addData("Status", "Running");
+            if (gamepad1.x) {
+                position = position - 0.0005;
+//                grab_servo.setPosition(position-2);
+//                if (position-2 < 5) {
+//                    grab_servo.setPosition(position+2);
+//                }
+            }
+            lift_servo.setPosition(position);
+            telemetry.addData("lift_Servo Position", position);
+//            telemetry.addData("Grab_Servo position", position);
+            telemetry.addLine("Status: Running");
             telemetry.update();
 
         }
