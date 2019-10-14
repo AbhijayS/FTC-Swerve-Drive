213d9c93558df4ab2b1d138f2a1a9f58700600d9
Cameron Shugart <shriketurret@gmail.com>
2019-10-14 14:48:44 -0500

    Update arm_test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm_test/arm_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm_test/arm_test.java
@@ -11,14 +11,14 @@ public class arm_test extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Servo arm_servo = hardwareMap.servo.get("s1");
         Servo grabber_servo = hardwareMap.servo.get("s2");
-        double arm_position = 1;
+        double arm_position = 0.751;
         double grabber_position = 0.5;
         waitForStart();
         while (opModeIsActive()){
             arm_servo.setPosition(arm_position);
             if (gamepad1.x){
-                if (arm_position==0.16) {
-                    arm_position = 0;
+                if (arm_position==0.751) {
+                    arm_position = 0.0455;
                 } else if (arm_position == 1) {
                     arm_position = 0.16;
                 } else {
