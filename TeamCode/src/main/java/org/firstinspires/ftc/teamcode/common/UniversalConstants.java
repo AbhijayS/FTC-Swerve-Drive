ae0ace75aa808db4c9b60cf2c7bb5ffcfc453ae9
Abhijay Saini <abhijay.saini@gmail.com>
2019-10-15 22:21:22 -0500

    ported existing code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/UniversalConstants.java
@@ -0,0 +1,236 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversalConstants {
+    public static final String rightFront = "rf";
+    public static final String rightRear = "rr";
+    public static final String leftFront = "lf";
+    public static final String leftRear = "lr";
+    public static final double maxDriveSpeed = 1;
+    public static final float joystickDeadzone = .1f;
+    public static final String vuforiaLicenceKey = "AfWYo6H/////AAABmQhOGpga9kzugmfBYrjJDQs2b+vIsT5Pjplu+VgGrxevhveFPbYqHKZeHrJKY1bhrSNzeogphVwdC2ndsu9sn9WZ/yOrOhQ9s8egFXfDzkPN6uhzqVo5A3qGAPK8CPqAcdraaJxMKINGPo+aBqbzELtMl+kX1BkUfbapRDYIBRS+oySt71kDzykpd0Iy7NgJGU5xfsEm8fJm9xWVTFV7IsChCAKeo7bmBUtcpIVR6smjqRT9noJcmWv2aP9J1C2CcW766ptLy0lKIl08M+S2uY8XYHjK2kBpOnHvkSrzsnrROgE/g7V4xDiFCEFBjk1w4qAcO5veTVzbhqxxSmWMQkb7JiORCUCZ+Dz5F8bYW5zY";
+
+    public enum Status {
+        DEBUGGING, TESTING, RELEASE
+    }
+
+    public enum SwerveState {
+        PATH_FOLLOWING,
+        PATH_FOLLOWING_COMPLETE,
+        HUMAN_INPUT
+    }
+
+    public static double roundTo2DecimalPlaces(double a) {
+        return Math.round(a * 100.0) / 100.0;
+    }
+
+    public enum MotionState {
+        STOPPED,
+        IN_MOTION,
+        ACCELERATING,
+        DECELERATING;
+    }
+
+    public enum Debugging {
+        RX("Robot X"),
+        RY("Robot Y"),
+        WA("Module0"),
+        HEADING("Heading"),
+        DISTANCE("Distance Delta"),
+        VELOCITY("Velocity"),
+        TRACE("Old TP X"),
+        SEG("Segment"),
+        PX("Tracking Point X"),
+        PY("Tracking Point Y"),
+        ERROR("Error"),
+        CORRECTION("Correction"),
+        PATH("Path");
+
+        private final String name;
+
+        Debugging(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public static List<String> getDebuggingMarkers() {
+            ArrayList<String> result = new ArrayList<>();
+            for (Debugging d : Debugging.values()) {
+                result.add(d.toString());
+            }
+            return result;
+        }
+
+    }
+
+    public static final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+
+    public enum MotorDirection {
+        FORWARD(1.0f, "Forward"),
+        REVERSE(-1.0f, "Reverse");
+
+        private final float sign;
+        private final String name;
+        private final String abbreviation;
+
+        MotorDirection(float sign, String name) {
+            this.sign = sign;
+            this.name = name;
+            if (name.length() > 0)
+                abbreviation = name.substring(0, 1);
+            else
+                abbreviation = "";
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public String getAbbreviation() {
+            return abbreviation;
+        }
+
+        public float getSign() {
+            return sign;
+        }
+
+        public MotorDirection getOpposite() {
+            if (this == MotorDirection.FORWARD)
+                return REVERSE;
+            return FORWARD;
+        }
+
+        public double assignDirection(double value) {
+            return Math.copySign(value, sign);
+        }
+    }
+
+    static final int CYCLE_MS = 50;
+    public static final Status ROBOT_STATUS = Status.TESTING;
+    public static final double ROBOT_WIDTH = 14.5; // INCHES
+    public static final double ROBOT_LENGTH = 15; // INCHES
+    public static final double ROBOT_MAX_SPEED = 0.05;
+    static final double ROBOT_COM_X = 0; // offset from (0,0)
+    static final double ROBOT_COM_Y = 0; // offset from (0,0)
+    static final double PI = Math.PI;
+    public static final double HALF_PI = PI / 2;
+
+    static final double TURN_FACTOR = 2; // Tune the robot's speed at turns (1 - 5)
+    public static final int servoRange = 270;      // 225° to -45° on the Unit Circle
+    public static final double servoSpeed = 0.16 / 60;  // sec/1° @ 5V TODO: Update speed for 6V input from the servo power module
+    public static final int servoDefaultAngle = 90;
+    public static final int ticksPerRevolution = 560;
+    public static final int wheelDiameter = 3; //inches
+    public static final double wheelCircumference = wheelDiameter * Math.PI; //inches
+
+    public static final double driveGearRatio = 24.0 / 18.0; // Output rate : Input rate
+    public static final double kS =   5; // Steering gain (path following only)
+    public static final double kP =   0; // Proportional gain
+    public static final double kI =   0; // Integral gain
+    public static final double kD =   0; // Derivative gain
+
+
+    public static final PwmControl.PwmRange pwmRange = new PwmControl.PwmRange(500, 2500);
+
+
+
+    public enum ModuleConfig {
+        MODULE_ZERO     ("Module ZERO",     (-ROBOT_WIDTH/2)-ROBOT_COM_X,(ROBOT_LENGTH/2)-ROBOT_COM_Y,     false,  "FLS",    0        ,  Servo.Direction.REVERSE,    "FLM",  MotorDirection.REVERSE)  ,
+        MODULE_ONE      ("Module ONE",      (ROBOT_WIDTH/2)-ROBOT_COM_X,(ROBOT_LENGTH/2)-ROBOT_COM_Y,      false,  "FRS",   -3.07    ,  Servo.Direction.FORWARD,    "FRM", MotorDirection.FORWARD)  ,
+        MODULE_TWO      ("Module TWO",      (ROBOT_WIDTH/2)-ROBOT_COM_X,(-ROBOT_LENGTH/2)-ROBOT_COM_Y,     false,  "RRS",    2.92     ,  Servo.Direction.FORWARD,    "RRM",  MotorDirection.FORWARD)  ,
+        MODULE_THREE    ("Module THREE",    (-ROBOT_WIDTH/2)-ROBOT_COM_X,(-ROBOT_LENGTH/2)-ROBOT_COM_Y,    false,  "RLS",     -4.43    ,  Servo.Direction.FORWARD,    "RLM",   MotorDirection.REVERSE)  ;
+
+        public final String                     moduleName;
+        public final String                     servoID;
+        public final double                     servoDelta;
+        public final Servo.Direction            servoDirection;
+        public final boolean                    isModuleDisabled;
+        public final String                     motorID;
+        public final double                     x, y;
+
+        public final MotorDirection    motorDirection;
+        ModuleConfig(String module_name, double x, double y, boolean is_module_disabled, String servo_id, double servo_delta, Servo.Direction servo_direction, String motor_id, MotorDirection motor_direction) {
+            this.moduleName         = module_name;
+            this.isModuleDisabled   = is_module_disabled;
+            this.servoID            = servo_id;
+            this.servoDelta         = servo_delta;
+            this.servoDirection     = servo_direction;
+            this.motorID            = motor_id;
+            this.motorDirection     = motor_direction;
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    /**
+     * @param a - any angle in degrees in the range (-infinity to infinity)
+     * @return Normalizes the angle in the range (-180 to 180)
+     */
+    public static double clipAngle(double a) {
+        return Math.toDegrees(Math.atan2(Math.sin(Math.toRadians(a)), Math.cos(Math.toRadians(a))));
+    }
+
+    public enum Direction {
+        FORWARD(1.0f,"Forward"),
+        REVERSE(-1.0f,"Reverse"),
+        RIGHT(1.0f, "Right"),
+        LEFT(-1.0f, "Left"),
+        UNKOWN(0.0f, "Unknown");
+
+        private final float sign;
+        private final String name;
+        private final String abbreviation;
+
+        Direction(float sign, String name) {
+            this.sign = sign;
+            this.name = name;
+            if (name.length() > 0)
+                abbreviation = name.substring(0,1);
+            else
+                abbreviation = "";
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public String getAbbreviation() {
+            return abbreviation;
+        }
+
+        public float getSign() {
+            return sign;
+        }
+
+        public Direction getOpposite() {
+            switch (this) {
+                case LEFT:
+                    return RIGHT;
+                case RIGHT:
+                    return LEFT;
+                case FORWARD:
+                    return REVERSE;
+                default:
+                    return FORWARD;
+            }
+        }
+
+        public double assignDirection(double value) {
+            return Math.copySign(value, sign);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/vision/TensorFlowLite.java
@@ -0,0 +1,300 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.common.UniversalConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TensorFlowLite {
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+    private Telemetry telemetry;
+    private LinearOpMode linearOpMode;
+    private HardwareMap hardwareMap;
+    private double confidence;
+    String pattern;
+
+
+    public void status(String s) {
+        telemetry.addLine(s);
+        telemetry.update();
+    }
+
+
+    /**
+     * Constructor for the TensorFlowLite object detection class
+     *
+     * @param l Input this to run using LinearOpMode functions
+     */
+    public TensorFlowLite(LinearOpMode l, double confidence) {
+        linearOpMode = l;
+        telemetry = linearOpMode.telemetry;
+        hardwareMap = linearOpMode.hardwareMap;
+        pattern = "Unknown";
+        this.confidence = confidence;
+
+        initVuforia();
+
+        status("Vuforia has been initialized.");
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+            status("Tensor Flow has been initialized.");
+        } else {
+            status("This device does not support Tensor Flow.");
+        }
+    }
+
+
+    /**
+     * This function activates the TensorFlow Object Detection engine
+     */
+    public void activateTfod() {
+        if (tfod != null) {
+            tfod.activate();
+        }
+    }
+
+
+    /**
+     * This function deactivates the TensorFlow Object Detection engine
+     */
+    public void shutDownTfod() {
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
+
+
+    /**
+     * This function is used to determine the SkyStone Arrangement Pattern.
+     * It will set the pattern variable to A, B or C depending on SkyStone arrangement.
+     * The function has a background elimination method based off of the one implemented in Rover Ruckus
+     */
+    public void determinePattern() {
+        if (tfod != null) {
+            List<Recognition> updateRecognitions = tfod.getUpdatedRecognitions();
+            if (updateRecognitions != null) {
+                telemetry.addData("Objects Detected", updateRecognitions.size());
+                if (updateRecognitions.size() >= 3) {
+                    int SkyStoneX = -1;
+                    int SkyStoneY = -1;
+                    int Stone1X = -1;
+                    int Stone1Y = -1;
+                    int Stone2X = -1;
+                    int Stone2Y = -1;
+
+                    ArrayList<Integer> SkystoneXvals = new ArrayList<Integer>();
+                    ArrayList<Integer> SkystoneYvals = new ArrayList<Integer>();
+                    ArrayList<Integer> StoneXvals = new ArrayList<Integer>();
+                    ArrayList<Integer> StoneYvals = new ArrayList<Integer>();
+                    for (Recognition recognition : updateRecognitions) {
+                        if (recognition.getLabel().equals(LABEL_SECOND_ELEMENT)) {
+                            SkystoneXvals.add((int) recognition.getLeft());
+                            SkystoneYvals.add((int) recognition.getBottom());
+                        } else {
+                            StoneXvals.add((int) recognition.getLeft());
+                            StoneYvals.add((int) recognition.getBottom());
+                        }
+                    }
+                    int indexToRemove = 0;
+                    for (int i = 0; i < SkystoneYvals.size(); i++) {
+                        if (SkyStoneY <= SkystoneYvals.get(i) || SkyStoneX < SkystoneXvals.get(i)) {
+                            SkyStoneY = SkystoneXvals.get(i);
+                            SkyStoneX = SkystoneXvals.get(i);
+                        }
+                    }
+
+                    for (int i = 0; i < StoneYvals.size(); i++) {
+                        if (Stone1Y < StoneYvals.get(i)) {
+                            Stone1Y = StoneYvals.get(i);
+                            Stone1X = StoneXvals.get(i);
+                            indexToRemove = i;
+                        }
+                    }
+                    if (StoneYvals.size() > 0) {
+                        StoneXvals.remove(indexToRemove);
+                        StoneYvals.remove(indexToRemove);
+                    }
+                    for (int i = 0; i < StoneYvals.size(); i++) {
+                        if (Stone2Y < StoneYvals.get(i)) {
+                            Stone2Y = StoneYvals.get(i);
+                            Stone2X = StoneXvals.get(i);
+                        }
+                    }
+                    if (SkyStoneX != -1 && Stone1X != -1 && Stone2X != -1) {
+                        if (SkyStoneX < Stone1X && SkyStoneX < Stone2X) {
+                            pattern = "A";
+                            telemetry.addData("Pattern", "A");
+                        } else if (SkyStoneX > Stone1X && SkyStoneX > Stone2X) {
+                            pattern = "C";
+                            telemetry.addData("Pattern", "C");
+                        } else {
+                            pattern = "B";
+                            telemetry.addData("Position", "B");
+                        }
+                    }
+                }
+                telemetry.addData("Pattern Variable: ", pattern);
+                telemetry.update();
+
+            }
+        }
+    }
+
+    public void updateTensorFlowSingle() {
+        if (tfod != null) {
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                // the following code was taken form FTC sample code and partially modified for our robot's purposes
+                // It is alright if not understood.
+                //x and y position values for minerals
+                int SkystoneX = -1;
+                int SkystoneY = -1;
+                double Azimuth = 0;
+                /* Creates an array list of x and y values for all minerals*/
+                ArrayList<Integer> SkyStoneXvals = new ArrayList<Integer>();
+                ArrayList<Integer> SkyStoneYvals = new ArrayList<Integer>();
+                ArrayList<Double> SkyStoneAzimuths = new ArrayList<Double>();
+                for (Recognition recognition : updatedRecognitions) {
+                    if (recognition.getLabel().equals(LABEL_SECOND_ELEMENT)) {
+                        SkyStoneXvals.add((((int) recognition.getLeft()) + ((int) recognition.getRight())) / 2);
+                        SkyStoneYvals.add((int) recognition.getBottom());
+                        SkyStoneAzimuths.add(recognition.estimateAngleToObject(AngleUnit.DEGREES));
+
+                    }
+                }
+                int indexToRemove = 0;
+                for (int i = 0; i < SkyStoneYvals.size(); i++) {
+                    if (SkystoneY < SkyStoneYvals.get(i)) {
+                        SkystoneY = SkyStoneYvals.get(i);
+                        SkystoneX = SkyStoneXvals.get(i);
+                        Azimuth = SkyStoneAzimuths.get(i);
+                    }
+                }
+                telemetry.addData("Gold X value: ", SkystoneX);
+                telemetry.addData("Azimuth: ", Azimuth);
+                if (SkystoneX != -1) {
+                    if (SkystoneX <= 426 && Azimuth <= -15) {
+                        pattern = "A";
+                        telemetry.addData("Position: ", "Left");
+                    } else if (SkystoneX >= 852 && Azimuth >= 15) {
+                        pattern = "C";
+                        telemetry.addData("Position: ", "Right");
+                    } else {
+                        pattern = "B";
+                        telemetry.addData("Position: ", "Center");
+                    }
+                }
+
+                telemetry.addData("Pattern Variable: ", pattern);
+                telemetry.update();
+            }
+        }
+    }
+
+
+    public void detectAbsence() {
+        if (tfod != null) {
+            List<Recognition> updateRecognitions = tfod.getUpdatedRecognitions();
+            if (updateRecognitions != null) {
+                int Stone1X = -1;
+                int Stone2X = -1;
+                for (Recognition recognition : updateRecognitions) {
+                    if (recognition.getLabel().equals(LABEL_FIRST_ELEMENT) && Stone1X == -1) {
+                        Stone1X = (int) recognition.getLeft();
+
+                    } else if (recognition.getLabel().equals(LABEL_FIRST_ELEMENT)) {
+                        Stone2X = (int) recognition.getLeft();
+                    }
+                }
+                if (Stone1X != -1 && Stone2X != -1) {
+                    if (Stone1X <= 852 && Stone2X <= 852) {
+                        pattern = "C";
+                        telemetry.addData("Pattern: ", "C");
+                    } else if (Stone1X >= 426 && Stone2X >= 426){
+                        pattern = "A";
+                        telemetry.addData("Pattern: ", "A");
+                    } else{
+                        pattern = "B";
+                        telemetry.addData("Pattern: ", "B");
+                    }
+
+                }
+            }
+            telemetry.addData("Pattern Variable: ", pattern);
+            telemetry.update();
+        }
+    }
+
+
+    /**
+     * Acccesses the private variable pattern which is set by determinePattern() function
+     *
+     * @return pattern
+     */
+    public String getPattern() {
+        return pattern;
+    }
+
+    /**
+     * This function initializes Vuforia to use camera on phones for object detection.
+     */
+    private void initVuforia() {
+        //This method will create all neccessary vuforia parameters and set them
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = UniversalConstants.vuforiaLicenceKey;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+
+    /**
+     * This function initializes Vuforia to use an external USB camera for object detection.
+     */
+    private void initVuforiaWebCam() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = UniversalConstants.vuforiaLicenceKey;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
+    }
+
+
+    /**
+     * This function loads all necessary information into the TensorFlow Object Detection Engine to use for image recognition.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = confidence;
+
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        //Change these parameters when loading a new model asset for object detection. Set appropriate labels
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+        tfod.setClippingMargins(0,100,0,100);
+    }
+}
