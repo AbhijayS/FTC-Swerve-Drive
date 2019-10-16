8009629e3a90c51ffd3dec33b627865a7cc63e48
Gabriel Brown <Gabebrown2023@gmail.com>
2019-10-16 17:32:59 -0500

    deleted a copy of a file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm_test/arm_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm_test/arm_test.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.arm_test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
