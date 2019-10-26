7c41e65b91a919780706dda49f1145581f089710
FlxMurdo2019 <52217865+FlxMurdo2019@users.noreply.github.com>
2019-10-26 13:48:56 -0500

    removed uneeded stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/jewelSwatter/armTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/jewelSwatter/armTester.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.modules.jewelSwatter;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name = "armTester")
 
