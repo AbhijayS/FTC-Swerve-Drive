0b7df31405c516c05a4b7cfb466ca9824b7bd938
Abhijay Saini <abhijay.saini@gmail.com>
2019-11-03 16:54:35 -0600

    Create WayPoint.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/utilities/WayPoint.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.common.utilities;
+
+import org.firstinspires.ftc.teamcode.modules.elevator.Clamp;
+import org.firstinspires.ftc.teamcode.modules.jewelswatter.JewelSwatter;
+
+public class WayPoint {
+    Path path;
+    JewelSwatter.JewelSwatterState jewelSwatterState;
+    
+}
