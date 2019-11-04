ea2271923690eb85063d6614b4ddc5761f992c94
Yazan <yazan@Yazan-MacBook-Air.local>
2019-11-04 16:48:49 -0600

    Alex only uses the nipple

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FullTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FullTeleOp.java
@@ -2,13 +2,8 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.common.utilities.Gamepad;
-import org.firstinspires.ftc.teamcode.modules.elevator.Clamp;
 import org.firstinspires.ftc.teamcode.modules.elevator.Elevator;
 import org.firstinspires.ftc.teamcode.modules.swerve.SwerveDrive;
 
@@ -17,37 +12,27 @@ public class FullTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SwerveDrive swerveDrive = new SwerveDrive(this, null);
-        Clamp clamp = new Clamp(this);
+        Elevator elevator = new Elevator(this);
+        //Clamp clamp = new Clamp(this);
         Gamepad gamepad = new Gamepad(this);
+        double liftPower = 0;
         gamepad.update();
 
-        DcMotor liftOne = (DcMotorEx) hardwareMap.dcMotor.get("L1");
-        DcMotor liftTwo = (DcMotorEx) hardwareMap.dcMotor.get("L2");
 
-        liftOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        liftOne.setDirection(DcMotorSimple.Direction.FORWARD);
-        liftTwo.setDirection(DcMotorSimple.Direction.FORWARD);
 
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive()){
-            clamp.updateByGamepad(gamepad);
-            telemetry.addData("Clamp Status", clamp.getStatus());
+            //clamp.updateByGamepad(gamepad);
+            telemetry.addData("Clamp Status", elevator.clamp.getStatus());
 
-            double power = gamepad.lift;
-            if (liftTwo.getCurrentPosition() <= 0 && power < 0) {
-                power = 0;
-            }
-            liftOne.setPower(power);
-            liftTwo.setPower(power);
+            elevator.updateByGamepad(gamepad, liftPower);
 
-            telemetry.addData("Position", liftTwo.getCurrentPosition());
             swerveDrive.fod(gamepad);
 //            swerveDrive.swerveKinematics.update();
             gamepad.update();
+            liftPower = gamepad.lift;
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/PotentiometerTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/PotentiometerTester.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.modules.sensory.Potentiometer;
 public class PotentiometerTester extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Potentiometer potentiometer = new Potentiometer("p",this);
+        Potentiometer potentiometer = new Potentiometer("P",this);
         waitForStart();
         while (opModeIsActive()){
             int angle = potentiometer.returnAngle();
