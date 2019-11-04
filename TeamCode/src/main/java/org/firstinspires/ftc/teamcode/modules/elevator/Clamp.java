57ef7b9365e2418bc82f53e52c63bc8e73cfd6a7
Yazan <yazan@Yazan-MacBook-Air.local>
2019-11-04 17:18:59 -0600

    Update Clamp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/elevator/Clamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/elevator/Clamp.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.modules.elevator;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.common.utilities.Direction;
 import org.firstinspires.ftc.teamcode.common.utilities.Gamepad;
+import org.firstinspires.ftc.teamcode.modules.sensory.Potentiometer;
 
 import static org.firstinspires.ftc.teamcode.common.UniversalConstants.clampServo;
 
@@ -18,6 +17,7 @@ public class Clamp {
     private CRServo servo;
     private double maxPower = 1;
     private int enable; // -1 = unclamp, 1 = clamp, 0 = no movement
+    private Potentiometer potentiometer;
 
     public Clamp(LinearOpMode l) {
         linearOpMode = l;
@@ -25,16 +25,24 @@ public class Clamp {
         HardwareMap hardwareMap = l.hardwareMap;
         servo = hardwareMap.crservo.get(clampServo);
         servo.setDirection(DcMotorSimple.Direction.REVERSE);
+        potentiometer = new Potentiometer("P", linearOpMode);
         enable = 1;
+
     }
 
     public void updateByGamepad(Gamepad g) {
+        //64-100
         if (g.clamp) {
             this.enable = 1;
         } else if (g.unclamp){
             this.enable = -1;
         } else {
             this.enable = 0;
+        }
+        if(potentiometer.returnAngle() <= 64 && enable < 0){
+            enable = 0;
+        }else if(potentiometer.returnAngle() >= 99 && enable >0){
+            enable = 0;
         }
 
         servo.setPower(enable * maxPower);
