7dcef8cffc7f0a07a8a5f9ce1876cae87d5f996f
Gabriel Brown <Gabebrown2023@gmail.com>
2019-12-01 13:44:16 -0600

    Hard Stops

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/HardStopTester.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.testers;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.modules.hardStops;
+
+@TeleOp(name = "hardStopTester")
+public class HardStopTester extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        hardStops stops = new hardStops(this);
+        if(this.gamepad2.a){
+            stops.setState(hardStops.hardStopState.STOW);
+        }
+        else if(this.gamepad2.b){
+            stops.setState(hardStops.hardStopState.PARTIAL_DEPLOY);
+        }
+        else if(this.gamepad2.x){
+            stops.setState(hardStops.hardStopState.DEPLOY);
+        }
+        telemetry.addData("Stop State: ", stops.getState());
+        telemetry.update();
+    }
+
+}
