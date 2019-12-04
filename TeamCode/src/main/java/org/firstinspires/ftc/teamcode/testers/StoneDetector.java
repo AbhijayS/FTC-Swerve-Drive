9f7cb47bb175f9fef0411704e2e5756e0b7d8887
Abhijay Saini <abhijay.saini@gmail.com>
2019-12-03 23:07:48 -0600

    conversion working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/StoneDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/StoneDetector.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode.testers;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.vuforia.Frame;
+import com.vuforia.Image;
 import com.vuforia.PIXEL_FORMAT;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.common.UniversalConstants;
 
@@ -17,7 +17,6 @@ import org.firstinspires.ftc.teamcode.common.UniversalConstants;
 public class StoneDetector extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, UniversalConstants.webcamName);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = UniversalConstants.vuforiaLicenceKey;
@@ -25,26 +24,42 @@ public class StoneDetector extends LinearOpMode {
 
         VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
         vuforia.enableConvertFrameToFormat(PIXEL_FORMAT.GRAYSCALE);
-        vuforia.setFrameQueueCapacity(1);
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565,true);
+        vuforia.setFrameQueueCapacity(2);
 
-        Bitmap bitmap = vuforia.convertFrameToBitmap(vuforia.getFrameQueue().take());
-        telemetry.addData("Height", bitmap.getHeight());
-        telemetry.addData("Width", bitmap.getWidth());
-
+        telemetry.addLine("Ready to go!");
+        telemetry.update();
         waitForStart();
 
+        // get image from vuforia
+        Frame frame = vuforia.getFrameQueue().take();
+        telemetry.addData("Vu Height", frame.getImage(1).getHeight());
+        telemetry.addData("Vu Width", frame.getImage(1).getWidth());
+        telemetry.addData("Vu Format", frame.getImage(1).getFormat() == PIXEL_FORMAT.RGB565);
+
+        // build an android bitmap
+        Image vu_img = frame.getImage(1);
+        Bitmap bitmap = Bitmap.createBitmap(vu_img.getWidth(), vu_img.getHeight(), Bitmap.Config.RGB_565);
+        bitmap.copyPixelsFromBuffer(vu_img.getPixels());
+
+        telemetry.addLine();
+        telemetry.addData("Bit Height", bitmap.getHeight());
+        telemetry.addData("Bit Width", bitmap.getWidth());
+        telemetry.addData("Pixel", bitmap.getPixel(0,0));
+        telemetry.addData("Gray", rgbToGray(bitmap.getPixel(0,0)));
+
+        telemetry.update();
         while (opModeIsActive()) {
-            telemetry.update();
+            idle();
         }
     }
 
 
-    public int rgbToGrey(int color) {
+    public int rgbToGray(int color) {
         int R = (color >> 16) & 0xff;
         int G = (color >>  8) & 0xff;
         int B = (color      ) & 0xff;
-//        (0.3 * R) + (0.59 * G) + (0.11 * B)
-        int result = (int) Math.round(0.3*R + 0.59*G + 0.11*B);
-        return result;
+        int gray = (int) Math.round(0.3*R + 0.59*G + 0.11*B);
+        return gray;
     }
 }
