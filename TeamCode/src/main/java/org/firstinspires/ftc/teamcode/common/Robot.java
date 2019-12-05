b540cec85322d3df80aa87aeae616477c9dc7ba8
Abhijay Saini <abhijay.saini@gmail.com>
2019-12-05 12:13:42 -0600

    use center of mass instead of path segment for PID movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Robot.java
@@ -148,7 +148,7 @@ public class Robot {
                         } catch (Exception e) {
                             throw new RuntimeException(e.getMessage());
                         }
-                        double distance = path.SEGMENT_END.Y - path.SEGMENT_START.Y;
+                        double distance = swerveDrive.swerveKinematics.getCenterOfMass().getY() - path.SEGMENT_START.Y;
                         swerveDrive.movePID(distance, currentWaypoint.POWER);
                         swerveDrive.enablePID();
                         pidOverride = true;
