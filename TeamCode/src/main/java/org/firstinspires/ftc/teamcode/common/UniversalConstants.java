596401a1a1e6400e2a9ca41813061114607fce95
Abhijay Saini <abhijay.saini@gmail.com>
2019-12-05 20:44:19 -0600

    adequacy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/UniversalConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/UniversalConstants.java
@@ -103,7 +103,7 @@ public class UniversalConstants {
 
     public enum JewelSwatterConfig {
         LEFT("JSLA1", "JSLA2", Servo.Direction.FORWARD, Servo.Direction.REVERSE, 0),
-        RIGHT("JSRA1", "JSRA2", Servo.Direction.FORWARD, Servo.Direction.FORWARD, 0);
+        RIGHT("JSRA1", "JSRA2", Servo.Direction.REVERSE, Servo.Direction.FORWARD, 0);
 
         public String nameAxis1, nameAxis2;
         public double delta;
