335011135801fc24b256f0abb5ee6ce0690560f5
Abhijay Saini <abhijay.saini@gmail.com>
2019-12-09 07:24:28 -0600

    Update FastLift.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/FastLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/FastLift.java
@@ -10,32 +10,36 @@ public class FastLift extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor liftOne = hardwareMap.dcMotor.get("L1");
-        double power;
-//        int tp;
+        DcMotor liftTwo = hardwareMap.dcMotor.get("L2");
 
         liftOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         liftOne.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftTwo.setDirection(DcMotorSimple.Direction.FORWARD);
+
         liftOne.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftTwo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftTwo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addLine("Ready to go!");
+        telemetry.update();
 
         waitForStart();
 
-        power = 0.05;
-//        tp = (int) convertToTicks(5);
-        liftOne.setPower(power);
-//        liftOne.setTargetPosition(tp);
-
         while (opModeIsActive()) {
-            idle();
+            double power = -gamepad2.left_stick_y;
+            liftOne.setPower(power);
+            liftTwo.setPower(power);
+            telemetry.addData("Power", power);
+            telemetry.update();
         }
     }
-
-    public double convertToTicks(double inches) {
-        double tickValue = 0;
-        double linear_conversion = inches / 4.0;
-        tickValue = linear_conversion * (1.0 / (2.3 * Math.PI)) * 62 * (38.0 / 62.0) * (1.0 / 38.0) * 560.0;
-        return tickValue;
-    }
+//    public double convertToTicks(double inches) {
+//        double tickValue = 0;
+//        double linear_conversion = inches / 4.0;
+//        tickValue = linear_conversion * (1.0 / (2.3 * Math.PI)) * 62 * (38.0 / 62.0) * (1.0 / 38.0) * 560.0;
+//        return tickValue;
+//    }
 }
